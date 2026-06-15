66527d243fb74bedc811c2e8a3f61fecfbc68928
QwertyRyo <61341858+QwertyRyo@users.noreply.github.com>
2026-06-15 16:44:29 -0700

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Crew_Panic_Customizer")]
+[assembly: AssemblyDescription("A mod that lets you set crew emotional levels")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Qwertyryo")]
+[assembly: AssemblyProduct("Crew_Panic_Customizer")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d3c618e-9e96-40be-b086-63906f6958a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
